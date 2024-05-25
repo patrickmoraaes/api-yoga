1df5c3cec7983900034cb075ddc8609f31bbce2a
Gabriel Araujo Alves <gabreel.contato@gmail.com>
2024-05-25 19:48:19 -0300

    adicionando mensagem de erro ao errar login

--- Front-end/login/login.js
+++ Front-end/login/login.js
@@ -1,10 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
+    const form = document.getElementById('loginForm');
+    const errorMessageDiv = document.getElementById('error-message');
+    const loadingDiv = document.getElementById('loading');
+
+    const hideErrorMessage = () => {
+        errorMessageDiv.style.display = 'none';
+        errorMessageDiv.textContent = '';
+    };
+
+    // Adicionar evento de foco aos inputs para ocultar a mensagem de erro
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.addEventListener('focus', hideErrorMessage);
+    });
+
     form.addEventListener('submit', async (event) => {
         event.preventDefault(); // Evita o envio padrão do formulário
 
         const email = form.elements['email'].value; 
         const password = form.elements['senha'].value; 
+
+        // Limpar mensagens de erro e carregamento antes de nova tentativa
+        hideErrorMessage();
+        loadingDiv.style.display = 'none';
+
         try {
             const response = await fetch('http://localhost:8080/users/login', {
                 method: 'POST',
@@ -16,18 +35,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (response.ok) {
                 // Login bem-sucedido
-                alert('Login bem-sucedido!');
-                window.location.href = '../yoga1.html';
+                loadingDiv.style.display = 'block'; // Mostrar o carregamento
+                setTimeout(() => {
+                    window.location.href = '../yoga1.html';
+                }, 2000); // Espera 2 segundos para simular o carregamento
             } else {
                 // Falha no login
                 const data = await response.json();
-                alert(`Falha no login: ${data}`);
+                errorMessageDiv.textContent = 'Email ou login incorretos';
+                errorMessageDiv.style.display = 'block';
             }
-        }catch (error) {
-
+        } catch (error) {
             console.error('Erro:', error);
-            alert('Erro ao tentar fazer login. Tente novamente mais tarde.');
+            errorMessageDiv.textContent = 'Erro ao tentar fazer login. Verifique o email ou senha';
+            errorMessageDiv.style.display = 'block';
         }
-        
     });
 });
